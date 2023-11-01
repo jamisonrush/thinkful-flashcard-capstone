ca8c2bed200aa9916b9a6ba2e9bebbdc13e82bea
jamisonrush <103677258+jamisonrush@users.noreply.github.com>
2023-11-01 11:33:10 -0700

    Update EditDeck.js

--- src/deck/EditDeck.js
+++ src/deck/EditDeck.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import Stepper from "../Layout/Stepper";
 import { updateDeck, readDeck, listDecks } from "../utils/api/index";
+import DeckForm from "./DeckForm";
 
 function EditDeck({ decks, setDecks }) {
   const history = useHistory();
@@ -68,44 +69,12 @@ function EditDeck({ decks, setDecks }) {
       />
       <div className="container deck">
         <h1>Edit Deck</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="form-element">
-            <label htmlFor="name" className="form-label" />
-            Name:
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              placeholder="Deck Name"
-              className="form-control"
-            />
-          </div>
-          <div className="form-element">
-            <label htmlFor="desc" className="form-label" />
-            Description:
-            <textarea
-              id="desc"
-              name="desc"
-              value={formData.desc}
-              onChange={handleChange}
-              placeholder="Brief description of the deck"
-              className="form-control"
-            />
-          </div>
-          <div className="button-group">
-            <button
-              onClick={onClickCancel}
-              className="btn btn-outline-danger"
-            >
-              Cancel
-            </button>
-            <button type="submit" className="btn btn-outline-success">
-              Submit
-            </button>
-          </div>
-        </form>
+        <DeckForm
+          formData={formData}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+          onCancelClick={onClickCancel}
+        />
       </div>
     </div>
   );
